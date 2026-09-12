275228e8cf47e58b8a09dba7ff046c6a4342ff42
Joel van Ingen <j.c.ingen@gmail.com>
2026-09-12 22:33:01 +0200

    feat: basic bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Load env vars
+    dotenvy::dotenv().expect("Expected a .env file");
+
+    // Login
+    let token = env::var("BOT_TOKEN").expect("Expected a Discord token in the environment");
+
+    // Intents
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    
+    // Create client instance
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
